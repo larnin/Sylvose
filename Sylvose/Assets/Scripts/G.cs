de95a832b8d8f1915e918186643a5b57f6819f31
Nicolas <nicolas-donald.laurent@laposte.net>
2017-03-21 17:08:15 +0100

    Add base scripts

---
+++ Sylvose/Assets/Scripts/G.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public sealed class G
+{
+    private static volatile G instance;
+
+    public static G Sys
+    {
+        get
+        {
+            if (G.instance == null)
+                G.instance = new G();
+            return G.instance;
+        }
+    }
+
+}
