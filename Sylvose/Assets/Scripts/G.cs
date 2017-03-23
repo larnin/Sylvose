bacf3fe0198ecf58d710b2a6e6d4b061e8eb701a
Nicolas <nicolas-donald.laurent@laposte.net>
2017-03-23 15:49:13 +0100

    Basic player comportement

--- Sylvose/Assets/Scripts/G.cs
+++ Sylvose/Assets/Scripts/G.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public sealed class G
 {
-    private static volatile G instance;
+    private static volatile G _instance;
+    private PlayerComportement _playerComportement;
 
     public static G Sys
     {
         get
         {
-            if (G.instance == null)
-                G.instance = new G();
-            return G.instance;
+            if (G._instance == null)
+                G._instance = new G();
+            return G._instance;
         }
+    }
+
+    public PlayerComportement playerComportement
+    {
+        set
+        {
+            if (_playerComportement != null)
+                Debug.Log("2 PlayerComportement instanciated !");
+            _playerComportement = value;
+        }
+        get { return _playerComportement; }
     }
 
 }
